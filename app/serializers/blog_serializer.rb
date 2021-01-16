28a382fe98adffed43ebad08e5ed9a38e8a14b6f
gamil91 <alexis_gamil99@yahoo.com.com>
2021-01-15 21:29:56 -0800

    full CRUD on blogs

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,6 +1,23 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at
+  attributes :id, :title, :content, :private, :comments, :created_at, :user, :user_likes, :user_id
   has_many :comments
+  has_many :likes
   belongs_to :user
-  has_many :user_likes, through: :comments, source: :user
+  has_many :user_likes, through: :likes, source: :user
+
+  # attribute :user_likes do
+  #   object.user.name
+  # end
+  attribute :created_at do
+    object.created_at.strftime("%d %b %Y")
+  end
+
+  attribute :user do 
+    object.user.name
+  end
+
+  attribute :user_id do
+    object.user.id
+  end
+
 end
