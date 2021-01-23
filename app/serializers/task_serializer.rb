cecc970baaca31a5b30390372885b6b3920c7a8c
gamil91 <alexis_gamil99@yahoo.com.com>
2021-01-22 22:14:45 -0800

    working on CRUD on two new models

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,10 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :finished
-  has_one :list
+  attributes :id, :name, :finished, :list_id
+  # has_one :list
+
+  attribute :list_id do
+    object.list.id
+  end
+
+
 end
